23a2f3c21157d58fb3be500a44f9f085152afe12
Loki <Loki@fakeEmail.com>
2013-08-27 02:14:41 +0200

    Generate application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ploutos::Application.config.secret_key_base = '3c0d60752bc39e47f53d59e36048a60db939fc13e8e8c44db5ea61e4084d819e874b616fd36ae829e4b27692f530a85ef83a34a87a8e8dbfee64a2409d072951'
